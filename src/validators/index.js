99f0c4a47ba5091e84eb2c56bb629bd5f5917684
kvartborg <hello@kvartborg.net>
2017-12-07 15:49:12 +0100

    add benchmark test to get the operation time in ns

---
+++ src/validators/index.js
@@ -0,0 +1,9 @@
+import joi from './joi'
+import speclaValidator from './specla-validator'
+import superstruct from './superstruct'
+
+export default {
+  joi,
+  '@specla/validator': speclaValidator,
+  superstruct
+}
--- test/benchmark.test.js
+++ test/benchmark.test.js
@@ -1,10 +1,13 @@
 /* eslint-env jest */
-import speclaValidator from '../src/specla-validator'
-import superstruct from '../src/superstruct'
-import joi from '../src/joi'
+import validators from '../src/validators'
+import speclaValidator from '../src/validators/specla-validator'
+import superstruct from '../src/validators/superstruct'
+import joi from '../src/validators/joi'
 import simpleData from './data/simple-data'
 
-const run = (testFn, data, times = 500000) => {
+const testRuns = 500000
+
+const run = (testFn, data, times = testRuns) => {
   for (let i = 0; i < times; i++) testFn(data)
 }
 
@@ -18,4 +21,35 @@ test('Benchmark test @specla/validator', () => {
 
 test('Benchmark test joi', () => {
   run(joi, simpleData)
+})
+
+test('Benchmark avarage operation time in ns', () => {
+  const result = {}
+
+  const run = (validator, data) => {
+    const NS_PER_SEC = 1e9
+    let start, time
+    start = process.hrtime()
+    validator(data)
+    time = process.hrtime(start)
+    return time[0] * NS_PER_SEC + time[1]
+  }
+
+  for (const key in validators) {
+    const data = []
+
+    for (let i = 0; i < testRuns; i++) {
+      data.push(run(validators[key], simpleData))
+    }
+
+    result[key] = Math.round(data.reduce((a, b) => a + b, 0) / data.length)
+  }
+
+  console.log(
+    'Avarage operation time:',
+    Object.keys(result).reduce(
+      (acc, key) => acc + `\n  ${key}: ${result[key]} ns`,
+      ''
+    )
+  )
 })
